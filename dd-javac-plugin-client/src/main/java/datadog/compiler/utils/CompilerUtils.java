41d1592052671c0875c0f8c5f932458444c71761
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2023-01-17 12:44:58 +0100

    fixing build process

--- dd-javac-plugin-client/src/main/java/datadog/compiler/utils/CompilerUtils.java
+++ dd-javac-plugin-client/src/main/java/datadog/compiler/utils/CompilerUtils.java
@@ -6,9 +6,12 @@ public class CompilerUtils {
 
     /**
      * Returns path to class source file (injected by Datadog Java compiler plugin)
+     *
+     * @param clazz The class to get the source file path for
+     * @return The absolute path to the source code of the provided class
      */
-    public static String getSourcePath(Class<?> c) {
-        SourcePath sourcePathAnnotation = c.getAnnotation(SourcePath.class);
+    public static String getSourcePath(Class<?> clazz) {
+        SourcePath sourcePathAnnotation = clazz.getAnnotation(SourcePath.class);
         return sourcePathAnnotation != null ? sourcePathAnnotation.value() : null;
     }
 }
